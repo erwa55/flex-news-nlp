68569720f5da7c0beb5c65f39ccce78dea44bd3c
Erwan KERFOURN <erwankerfourn@icloud.com>
2023-12-28 13:52:23 -0800

    wrapping001

--- nlp/app/classify.py
+++ nlp/app/classify.py
@@ -1,12 +1,26 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from transformers import pipeline
 
+app = FastAPI()
+
+# Initialize the classifier
 classifier = pipeline("zero-shot-classification", model="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli")
-sequence_to_classify = "Angela Merkel is a politician in Germany and leader of the CDU"
-candidate_labels = ["politics", "economy", "entertainment", "environment"]
-output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
 
-# Pair each label with its score and find the label with the highest score
-highest_score_label = max(zip(output['labels'], output['scores']), key=lambda pair: pair[1])[0]
+class ClassificationRequest(BaseModel):
+    sequence_to_classify: str
+    candidate_labels: list
 
-# Print the label with the highest score
-print(highest_score_label)
+@app.post("/classify")
+async def classify(request: ClassificationRequest):
+    try:
+        # Run the classifier using the request content
+        output = classifier(request.sequence_to_classify, request.candidate_labels, multi_label=False)
+        # Find the label with the highest score
+        highest_score_label = max(zip(output['labels'], output['scores']), key=lambda pair: pair[1])[0]
+        return {"label": highest_score_label}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Run the API with uvicorn
+# uvicorn.run(app, host="0.0.0.0", port=8000)
